0a58a483443c0d6432ab160aaa8bd2bb7e4ed524
savchuksavchuk <sav4uk.oleg@gmail.com>
2025-12-19 15:49:32 +0200

    fix: remove tesseract

--- ai-summarizer-api/src/common/pdf/pdf.service.ts
+++ ai-summarizer-api/src/common/pdf/pdf.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { getDocument, PDFDocumentProxy } from 'pdfjs-dist/legacy/build/pdf.js';
 import { PDFPageProxy } from 'pdfjs-dist/types/web/interfaces';
-import { createCanvas } from 'canvas';
-import { createWorker } from 'tesseract.js';
 
 @Injectable()
 export class PdfService {
-  private readonly textSizeThreshold = 500;
-
   async getPagesCount(data: Uint8Array) {
     const loadingTask = getDocument({ data });
     const pdfDocument = await loadingTask.promise;
@@ -22,15 +18,7 @@ export class PdfService {
     const page = await pdfDocument.getPage(pageNum);
 
     try {
-      const text = await this.extractTextContentByPage(page);
-
-      if (text.length >= this.textSizeThreshold) {
-        return text;
-      }
-
-      const ocrText = await this.extractImagesContentByPage(page);
-
-      return `${text}\n${ocrText}`.trim();
+      return await this.extractTextContentByPage(page);
     } finally {
       page.cleanup();
     }
@@ -43,30 +31,5 @@ export class PdfService {
       .map((item: any) => item.str)
       .filter(Boolean)
       .join(' ');
-  }
-
-  private async extractImagesContentByPage(
-    page: PDFPageProxy,
-  ): Promise<string> {
-    const viewport = page.getViewport({ scale: 2 });
-
-    const canvas = createCanvas(viewport.width, viewport.height);
-    const ctx = canvas.getContext('2d');
-
-    await page.render({
-      canvasContext: ctx as any,
-      viewport,
-    }).promise;
-
-    const imageBuffer = canvas.toBuffer('image/png');
-
-    const worker = await createWorker();
-    const {
-      data: { text },
-    } = await worker.recognize(imageBuffer);
-
-    await worker.terminate();
-
-    return text;
   }
 }
